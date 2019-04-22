72da7af1b1194b8148845ce0ab800f9e1eba43a3
ryt6758 <tyr0412utx.dall@gmail.com>
2019-04-22 20:40:06 +0900

    express-app

--- app.js
+++ app.js
@@ -1,8 +1,46 @@
-var http = require('http'); //httpモジュール呼び出し
-var server = http.createServer(function (request, response) {
-  // リクエストを受けると以下のレスポンスを送信する
-  response.writeHead(200, { 'Content-Type': 'text/plain' }); //レスポンスヘッダーに書き込み
-  response.write('Hello World\n'); // レスポンスボディに「Hello World」を書き込み
-  response.end(); // レスポンス送信を完了する
+/* 1. expressモジュールをロードし、インスタンス化してappに代入。*/
+var express = require("express");
+var app = express();
+
+app.set('view engine', 'ejs');
+
+/* 2. listen()メソッドを実行して3000番ポートで待ち受け。*/
+var server = app.listen(8080, function () {
+  console.log("Node.js is listening to PORT:" + server.address().port);
 });
-server.listen(process.env.PORT || 8080);  //8080番ポートで待ち受け
+
+/* 3. 以後、アプリケーション固有の処理 */
+
+// 写真のサンプルデータ
+var photoList = [
+  {
+    id: "001",
+    name: "photo001.jpg",
+    type: "jpg",
+    dataUrl: "https://ic4-a.wowma.net/mis/gr/114/image.wowma.jp/31106436/ntc/kokoroes/cabinet/s227/5866187.jpg"
+  }, {
+    id: "002",
+    name: "photo002.jpg",
+    type: "jpg",
+    dataUrl: "https://pug1.net/pughoney/wp-content/uploads/2016/08/ra9.jpg"
+  }
+]
+
+app.get("/", function (req, res, next) {
+  res.render("index", {}); //{}はviewに対して、データを遅れる{photo: photolist}
+});
+
+// 写真リストを取得するAPI
+app.get("/api/photo/list", function (req, res, next) {
+  res.json(photoList);
+});
+
+app.get("/api/photo/:photoId", function (req, res, next) {
+  var photo;
+  for (i = 0; i < photoList.length; i++) {
+    if (photoList[i].id === req.params.photoId) {
+      var photo = photoList[i];
+    }
+  }
+  res.json(photo);
+});
