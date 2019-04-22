a0432b97b480d607e72cdf0325b7bd87fdb53d68
ryt6758 <tyr0412utx.dall@gmail.com>
2019-04-22 20:44:48 +0900

    express-app

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 app.set('view engine', 'ejs');
 
 /* 2. listen()メソッドを実行して3000番ポートで待ち受け。*/
-var server = app.listen(8080, function () {
+var server = app.listen(process.env.PORT || 8080, function () {
   console.log("Node.js is listening to PORT:" + server.address().port);
 });
 
