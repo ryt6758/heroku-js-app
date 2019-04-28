e105aa9e6c02fe7848104702debed9d5ac2b446c
TomoakiTANAKA <tanaka1987@gmail.com>
2019-04-28 14:36:48 +0900

    modelのデータをexportを使って取り出せるように

--- models/photolist.js
+++ models/photolist.js
@@ -12,3 +12,21 @@ module.exports.photoList = [
     dataUrl: "https://pug1.net/pughoney/wp-content/uploads/2016/08/ra9.jpg"
   }
 ]
+
+class Hoge {
+  photoList = [
+    {
+      id: "001",
+      name: "photo001.jpg",
+      type: "jpg",
+      dataUrl: "https://ic4-a.wowma.net/mis/gr/114/image.wowma.jp/31106436/ntc/kokoroes/cabinet/s227/5866187.jpg"
+    }, {
+      id: "002",
+      name: "photo002.jpg",
+      type: "jpg",
+      dataUrl: "https://pug1.net/pughoney/wp-content/uploads/2016/08/ra9.jpg"
+    }
+  ]
+}
+
+export default Hoge;
